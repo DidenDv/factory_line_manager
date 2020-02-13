dc1703292d1d4244ec2cca642b89fdc903015b48
d.didenko <d.didenko@artjoker.ua>
2020-02-13 15:38:00 +0200

    test

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+      <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+      </ul>
+  </footer>
+);
+
+export default Footer;
